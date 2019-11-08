cf506cf852893d246ae6dd2cd80f17e9fc93ca77
Fernando Hiroma <fernandohiroma@gmail.com>
2019-11-08 04:02:53 -0300

    Agregado de ViewModel para el formulario de registro.

---
+++ AyudandoAlProjimo.Data/ViewModels/RegistroViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AyudandoAlProjimo.Data.ViewModels
+{
+    public class RegistroViewModel
+    {
+        [Required]
+        [Display(Name = "Nombre de usuario")]
+        public string UserName { get; set; }
+        [Required]
+        [Display(Name = "Nombre")]
+        [StringLength(50, ErrorMessage = "Ingrese un nombre", MinimumLength = 1)]
+        public string Nombre { get; set; }
+        [Required]
+        [Display(Name = "Apellido")]
+        [StringLength(50, ErrorMessage = "Ingrese un apellido", MinimumLength = 1)]
+        public string Apellido { get; set; }
+        [Required]
+        [Display(Name = "Fecha de Nacimiento")]
+        public DateTime FechaNacimiento { get; set; }
+        [Required]
+        [StringLength(20, ErrorMessage = "La contraseña debe tener al menos 8 caracteres", MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,20}$",
+         ErrorMessage = "Debe tener al menos una mayúscula y un número.")]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirme su contraseña")]
+        [Compare("Password", ErrorMessage = "La contraseña no coincide con la anterior.")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+    }
+}
