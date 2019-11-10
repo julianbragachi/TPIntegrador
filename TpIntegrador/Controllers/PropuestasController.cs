772ceb584abe0c2ec5d26bf4973c6b1f6b52465f
julianbragachi <julian.bragachi@gmail.com>
2019-11-10 18:51:23 -0300

    add agregar propuesta (WIP)

---
+++ TpIntegrador/Controllers/PropuestasController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AyudandoAlProjimo.Data;
+
+namespace TpIntegrador.Controllers
+{
+    public class PropuestasController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AgregarPropuesta()
+        {
+            Propuestas p = new Propuestas();
+
+            p.PropuestasDonacionesHorasTrabajo.Add(new PropuestasDonacionesHorasTrabajo());
+            p.PropuestasDonacionesInsumos.Add(new PropuestasDonacionesInsumos());
+            p.PropuestasDonacionesMonetarias.Add(new PropuestasDonacionesMonetarias());
+
+            return View(p);
+        }
+
+        [HttpPost]
+        public ActionResult AgregarPropuesta(Propuestas p)
+        {
+            return View(p);
+        }
+    }
+}
