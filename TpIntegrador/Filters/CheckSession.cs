4a166031fb701ff888a22f3ef82cce95eca1f8d6
GabrielGF <gabriel-falon@hotmail.com>
2019-11-10 20:48:02 -0300

    "Sesiones y permisos de acceso"

---
+++ TpIntegrador/Filters/CheckSession.cs
@@ -0,0 +1,26 @@
+﻿using AyudandoAlProjimo.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TpIntegrador.Controllers;
+
+namespace TpIntegrador.Filters
+{
+    public class CheckSession : ActionFilterAttribute
+    {
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            HttpContext ctx = HttpContext.Current;
+            if (HttpContext.Current.Session["ID"] == null)
+
+            {
+                filterContext.Result = new RedirectResult("~/Ingresar/Login");
+                return;
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
