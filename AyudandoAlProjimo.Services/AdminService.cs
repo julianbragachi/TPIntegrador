77be0b180ba74dbd4e9137e2abe0f452d773aab0
GabrielGF <gabriel-falon@hotmail.com>
2019-11-11 20:50:31 -0300

    .

---
+++ AyudandoAlProjimo.Services/AdminService.cs
@@ -0,0 +1,26 @@
+﻿using AyudandoAlProjimo.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AyudandoAlProjimo.Services
+{
+    public class AdminService
+    {
+        Entities ctx = new Entities();
+
+        public List<Denuncias> ListarDenuncias()
+        {
+            //var result = (from d in ctx.Denuncias
+            //                 join art in db.Articulos on kar.ArticuloID equals art.ArticuloID
+            //                 join bod in db.Bodegas on art.BodegaID equals bod.BodegaID
+
+            //                 where bod.BodegaID == busqueda
+            //                 select kar
+            //   );
+
+        }
+    }
+}
