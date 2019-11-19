7bac9fecefa7c2d96cfb4eac3f04dbc69de355e4
GabrielGF <gabriel-falon@hotmail.com>
2019-11-19 10:10:28 -0300

    "Acerca-de en progreso"

---
+++ TpIntegrador/Controllers/AcercaDeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TpIntegrador.Controllers
+{
+    [Route]
+    public class AcercaDeController : Controller
+    {
+
+        // eg.: /acerca-de
+        [Route("/Acerca-de")]
+        public ActionResult Index() 
+        {
+            return View("Index");
+        }
+    }
+}
